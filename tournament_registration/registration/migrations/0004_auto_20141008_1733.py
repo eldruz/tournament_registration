e57817cf308982e069ce7f87a8b58c8e7b682b6e
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-09 13:43:28 +0200

    corrected a slight error in migration 0004

--- tournament_registration/registration/migrations/0004_auto_20141008_1733.py
+++ tournament_registration/registration/migrations/0004_auto_20141008_1733.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import registration.validators
 
 
 class Migration(migrations.Migration):
@@ -15,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='tournament',
             name='game',
-            field=models.CharField(max_length=256, choices=[('2X', 'Super Street Fighter 2X'), ('USF4', 'Ultra Street Fighter 4'), ('3.3', 'Street Fighter III Third Strike')], verbose_name=b'Game played for this tournament.\n                               The games are chosen from a hardcoded list.', validators=[registration.validators.validate_game]),
+            field=models.CharField(max_length=256, choices=[('2X', 'Super Street Fighter 2X'), ('USF4', 'Ultra Street Fighter 4'), ('3.3', 'Street Fighter III Third Strike')], verbose_name=b'Game played for this tournament.\n                               The games are chosen from a hardcoded list.'),
         ),
     ]

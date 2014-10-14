5031f229505979191b46b7f35fc95a519403645c
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-14 10:21:35 +0200

    named the views

--- tournament_registration/registration/urls.py
+++ tournament_registration/registration/urls.py
@@ -7,9 +7,9 @@ from .views import PlayersPerTournamentList
 from .views import TournamentsPerPlayerList
 
 urlpatterns = patterns('',
-    url(r'^tournaments/(?P<tournament_id>[-_\w]+)/players$', PlayersPerTournamentList.as_view()),
-    url(r'^tournaments/(?P<pk>[-_\w]+)/$', TournamentDetail.as_view()),
-    url(r'^tournaments/$', TournamentList.as_view()),
-    url(r'^players/(?P<player_name>[-_\w]+)/$', TournamentsPerPlayerList.as_view()),
-    url(r'^players/$', PlayerList.as_view())
+    url(r'^tournaments/(?P<tournament_id>[-_\w]+)/players$', PlayersPerTournamentList.as_view(), name='players_per_tournament'),
+    url(r'^tournaments/(?P<pk>[-_\w]+)/$', TournamentDetail.as_view(), name='tournament_detail'),
+    url(r'^tournaments/$', TournamentList.as_view(), name='tournament_list'),
+    url(r'^players/(?P<player_name>[-_\w]+)/$', TournamentsPerPlayerList.as_view(), name='tournaments_per_player'),
+    url(r'^players/$', PlayerList.as_view(), name='player_list')
 )

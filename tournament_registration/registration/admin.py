fd84a9a3ce4be68507af20d13519f00196365b42
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-15 14:17:02 +0200

    added the Player model to the administration page

--- tournament_registration/registration/admin.py
+++ tournament_registration/registration/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Tournament
-from .models import Entry
+from .models import Tournament, Entry, Player
 
 admin.site.register(Tournament)
 admin.site.register(Entry)
+admin.site.register(Player)

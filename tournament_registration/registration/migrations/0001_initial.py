4717792761a3f61d066a97218e2dd343ebbc910a
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-20 12:11:21 +0200

    cleaned the migrations to a fresh start

--- tournament_registration/registration/migrations/0001_initial.py
+++ tournament_registration/registration/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import registration.models
 
 
 class Migration(migrations.Migration):
@@ -14,7 +15,17 @@ class Migration(migrations.Migration):
             name='Entry',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('player', models.CharField(max_length=256)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Player',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=256, verbose_name=b'Nickname of the player')),
+                ('team', models.CharField(max_length=128, verbose_name=b'Name of her team', blank=True)),
             ],
             options={
             },
@@ -24,26 +35,46 @@ class Migration(migrations.Migration):
             name='Tournament',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(unique=True, max_length=256, verbose_name=b'Tournament title.')),
-                ('game', models.CharField(max_length=256, verbose_name=b'Game played for this tournament.\n                            The games are chosen from a hardcoded list.', choices=[('2X', 'Super Street Fighter 2X'), ('USF4', 'Ultra Street Fighter 4'), ('3.3', 'Street Fighter III Third Strike')])),
-                ('date', models.DateTimeField(verbose_name=b'Date of the tournament')),
-                ('support', models.CharField(max_length=256, verbose_name=b'Support on which the tournament will be played.')),
-                ('nb_max', models.PositiveSmallIntegerField(verbose_name=b'Maximum number of participants.')),
-                ('price', models.PositiveSmallIntegerField(verbose_name=b'Entry fee for the tournament')),
+                ('title', models.CharField(max_length=256, verbose_name=b'Tournament title.')),
+                ('game', models.CharField(max_length=256, verbose_name=b'Game played for this tournament.\n                               The games are chosen from a hardcoded list.', choices=[('2X', 'Super Street Fighter 2X'), ('USF4', 'Ultra Street Fighter 4'), ('3.3', 'Street Fighter III Third Strike')])),
+                ('date', models.DateField(verbose_name=b'Date of the tournament')),
+                ('support', models.CharField(default='Unknown', max_length=256, verbose_name=b'Support on which the tournament will be played.')),
+                ('nb_max', models.PositiveSmallIntegerField(default=32, verbose_name=b'Max number of participants.')),
+                ('price', models.PositiveSmallIntegerField(default=0, verbose_name=b'Entry fee')),
                 ('nb_per_team', models.PositiveSmallIntegerField(default=1, verbose_name=b'Number of players per team.')),
             ],
             options={
             },
-            bases=(models.Model,),
+            bases=(registration.models.ValidateOnSaveMixin, models.Model),
         ),
         migrations.AlterUniqueTogether(
             name='tournament',
             unique_together=set([('title', 'date')]),
         ),
         migrations.AddField(
-            model_name='entry',
-            name='tournament_title',
-            field=models.ForeignKey(to='registration.Tournament', to_field=b'title'),
+            model_name='player',
+            name='registered_tournaments',
+            field=models.ManyToManyField(to='registration.Tournament', through='registration.Entry'),
             preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='player',
+            unique_together=set([('name', 'team')]),
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='player',
+            field=models.ForeignKey(to='registration.Player'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='tournament_id',
+            field=models.ForeignKey(to='registration.Tournament'),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='entry',
+            unique_together=set([('tournament_id', 'player')]),
         ),
     ]

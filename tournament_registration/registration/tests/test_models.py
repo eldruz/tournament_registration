f610e2d4ae4d39b6b4bc4691e598f53e8265c91e
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-21 11:40:02 +0200

    refined some model tests

--- tournament_registration/registration/tests/test_models.py
+++ tournament_registration/registration/tests/test_models.py
@@ -29,13 +29,18 @@ class TournamentTestCase(TestCase):
         self.assertEqual(tourney.id, 5)
 
     def test_cannot_create_duplicate(self):
-        """Only one (title, date) couple can exist in the database"""
         self.assertRaises(ValidationError,
                           Tournament.utilities.create_tournament,
                           title='Tournoi test',
                           game='USF4',
                           date=date.today(),
                           nb_max=32)
+        self.assertRaises(ValidationError,
+                          Tournament.utilities.create_tournament,
+                          title='Tournoi test',
+                          game='2X',
+                          date=date.today(),
+                          nb_max=64)
 
     def test_game_not_in_list(self):
         """Game field of the tournament is in the authorized games list"""
@@ -51,6 +56,12 @@ class PlayerTestCase(TestCase):
     def setUp(self):
         Player.utilities.create_player(name='Eldruz',
                                        team='3HC')
+
+    def test_player_unicity_constraint(self):
+        self.assertRaises(IntegrityError,
+                          Player.utilities.create_player,
+                          name='Eldruz',
+                          team='3HC')
 
     def test_player_creation(self):
         player = Player.objects.get(name='Eldruz',
@@ -105,6 +116,19 @@ class EntryTestCase(TestCase):
         self.assertEqual(tournament, entry.tournament_id)
         self.assertEqual(player, entry.player)
 
+    def test_is_tournament_full(self):
+        tourney = Tournament.utilities.create_tournament(
+            title='MORLAIX',
+            game='2X',
+            date=date.today(),
+            nb_max=1)
+        straw_player = Player.utilities.create_player(name='Dourssin')
+        self.assertFalse(Entry.utilities.is_tournament_full(tourney))
+        Entry.utilities.create_entry(tournament=tourney,
+                                     player=straw_player)
+        self.assertTrue(Entry.utilities.is_tournament_full(tourney))
+
+
     def test_maximum_number_of_players_registered(self):
         tournament = Tournament.objects.get(pk=100)
         straw_player = Player.utilities.create_player(name='Boulbin')
@@ -125,8 +149,4 @@ class EntryTestCase(TestCase):
                                      player=player)
         Entry.utilities.create_entry(tournament=tourney,
                                      player=player)
-        # self.assertRaises(IntegrityError,
-        #                   Entry.utilities.create_entry,
-        #                   tournament=tourney,
-        #                   player=player)
         self.assertEqual(Entry.objects.filter(tournament_id=8).count(),1)

d1b7027806e37a8e428c673c236e97315c4e6322
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-27 09:15:05 +0100

    gorun settings updated for the capitalism app

--- tournament_registration/settings.py
+++ tournament_registration/settings.py
@@ -1,4 +1,5 @@
 DIRECTORIES = (
     ('registration', 'DJANGO_CONFIGURATION=Test python manage.py test registration'),
+    ('capitalism', 'DJANGO_CONFIGURATION=Test python manage.py test capitalism')
 )
 IGNORE_EXTENSIONS = ('swp',)

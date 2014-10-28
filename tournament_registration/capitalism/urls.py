bd68d436f47e2d51f84619a419d505b0684d3b62
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-28 11:53:00 +0100

    forgot about some CRUD...

--- tournament_registration/capitalism/urls.py
+++ tournament_registration/capitalism/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, url
 
 from .views import TournamentProductListView, TournamentProductCreateView, \
-    TournamentProductDetailView
+    TournamentProductDetailView, TournamentProductDeleteView, \
+    TournamentProductUpdateView
 
 urlpatterns = patterns('',
     url(r'^tproduct/list/$',
         TournamentProductListView.as_view(), name='tournament_product_list'),
     url(r'^tproduct/create/$',
         TournamentProductCreateView.as_view(), name='tournament_product_create'),
-    url(r'^tproduct/(?P<slug>[-_\w]+)/$',
+    url(r'^tproduct/delete/(?P<slug>[-_\w]+)/$',
         TournamentProductDetailView.as_view(), name='tournament_product_detail'),
+    url(r'^tproduct/update/(?P<slug>[-_\w]+)/$',
+        TournamentProductUpdateView.as_view(), name='tournament_product_update'),
+    url(r'^tproduct/(?P<slug>[-_\w]+)/$',
+        TournamentProductDeleteView.as_view(), name='tournament_product_delete'),
 )
--- tournament_registration/capitalism/views.py
+++ tournament_registration/capitalism/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, DeleteView, \
+    UpdateView
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
 
 from .models import TournamentProduct
 
@@ -29,3 +31,25 @@ class TournamentProductCreateView(CreateView):
                 stock=tournament_product.stock
             )
         return HttpResponseRedirect(tournament_product.get_absolute_url())
+
+
+class TournamentProductUpdateView(UpdateView):
+    model = TournamentProduct
+    fields = ['tournament', 'price', 'stock']
+    template_name = 'capitalism/create_tournamentproduct.html'
+
+    def form_valid(self, form):
+        tournament_product = form.save(commit=False)
+        tournament_product = \
+            TournamentProduct.utilities.update_tournament_product(
+                product_id=tournament_product.pk,
+                price=tournament_product.price,
+                stock=tournament_product.stock
+            )
+        return HttpResponseRedirect(tournament_product.get_absolute_url())
+
+
+class TournamentProductDeleteView(DeleteView):
+    model = TournamentProduct
+    success_url = reverse_lazy('tournament_product_list')
+    template_name = 'capitalism/delete_tournament.html'

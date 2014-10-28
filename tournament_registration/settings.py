595646c7a4191f222ce18d0f2e028a7452031534
Andrés Saraos Luna <eldruz@outlook.com>
2014-10-28 12:14:58 +0100

    more verbose test ouput

--- tournament_registration/settings.py
+++ tournament_registration/settings.py
@@ -1,5 +1,5 @@
 DIRECTORIES = (
-    ('registration', 'DJANGO_CONFIGURATION=Test python manage.py test registration'),
-    ('capitalism', 'DJANGO_CONFIGURATION=Test python manage.py test capitalism')
+    ('registration', 'DJANGO_CONFIGURATION=Test python manage.py test -v 2 registration'),
+    ('capitalism', 'DJANGO_CONFIGURATION=Test python manage.py test -v 2 capitalism')
 )
 IGNORE_EXTENSIONS = ('swp',)
